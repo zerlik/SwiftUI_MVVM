49c3e3efe5a03d238d2982b512a622002627351f
A zerlik <andy.zerlik@gmail.com>
2025-11-23 20:31:54 +0300

    Initial Commit

---
+++ MuseumGid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MuseumGid
+//
+//  Created by andrew on 23.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MuseumGid/MuseumGidApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseumGidApp.swift
+//  MuseumGid
+//
+//  Created by andrew on 23.11.25.
+//
+
+import SwiftUI
+
+@main
+struct MuseumGidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
